84f242a299971a97e0713f286709eb129f40bb9d
Oliver Mansell <oliverwfmansell@gmail.com>
2019-06-03 16:42:25 +0100

    fix typo in app.js again

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use("/lessons", jsonParser, lessonsRouter);
 app.use("/upload", uploadRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 // error handling
